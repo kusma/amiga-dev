b811595c68f17e0f156fb50308f6879f54e302b1
Erik Faye-Lund <kusmabite@gmail.com>
2011-05-09 19:05:55 +0200

    build and install vasm
    modify the build script to also build the assembler

---
+++ src/build.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+
+export prefix=../..
+export TARGET=m68k
+export CPU=m68k
+export SYNTAX=mot
+
+make -C vbcc install &&
+make -C vasm install || {
+	echo "Could not build VASM" >&2
+	exit 1
+}
+
--- src/build_vbcc.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-export TARGET=m68k
-
-cd vbcc &&
-mkdir -p bin &&
-prefix=../.. make install
